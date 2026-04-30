aa919101eb576a961c17662b91e459297c1bce5c
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2026-04-30 09:56:22 +0200

    Second commit

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomArray(length, min, max) {
+    const arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(randomNumber(min, max));
+    }
+    return arr;
+}
+
 console.log(randomNumber(1, 100));
+console.log("-------------------");
+console.log("-------------------");
+console.log(randomArray(5, 1, 100));
